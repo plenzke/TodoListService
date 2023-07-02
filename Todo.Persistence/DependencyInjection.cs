5cb0291969de42ec37d67f99e1838209ed8606e3
plenzke <3njoyth3sil3nc399@gmail.com>
2023-07-02 21:59:25 +0900

    added DependencyInjection

---
+++ Todo.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Todo.Application.Interfaces;
+
+namespace Todo.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<TasksDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<ITasksDbContext>(provider => provider.GetService<TasksDbContext>());
+
+            return services;
+        }
+    }
+}
